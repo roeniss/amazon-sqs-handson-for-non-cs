1794acb5f310a5e30fb46d3b682878b93b548ef5
RoenissMoon <roeniss2@gmail.com>
2020-02-14 14:00:46 +0900

    edit script to run at ec2 initiating time

--- server_script.sh
+++ server_script.sh
@@ -1,15 +1,16 @@
-#!/usr/bin/env bash
-apt update -y
-apt upgrade -y
-apt dist-upgrade -y
-apt autoremove -y
+#!/bin/bash
+apt-get update -y
+apt-get upgrade -y
+# apt-get dist-upgrade -y
+# apt-get autoremove -y
 curl -sL https://deb.nodesource.com/setup_10.x | sudo bash -
-apt install -y nodejs
+apt-get install -y nodejs
 ln -s /usr/bin/nodejs /usr/bin/node
+cd /home/ubuntu
 git clone https://github.com/roeniss/amazon-sqs-handson-for-non-cs
 cd amazon-sqs-handson-for-non-cs
 npm install
-mv sqs_node.service /lib/systemd/system/sqs_node.service
+cp sqs_node.service /lib/systemd/system/sqs_node.service
+# systemctl daemon-reload
 systemctl start sqs_node
 systemctl enable sqs_node
-
