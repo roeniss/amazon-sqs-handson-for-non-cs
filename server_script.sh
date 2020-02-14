2a5d34494bf325615ac9f6ee62fcb38100d69869
RoenissMoon <roeniss2@gmail.com>
2020-02-14 13:00:50 +0900

    edit script to run server with systemctl

--- server_script.sh
+++ server_script.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
-sudo su
 apt update -y
 apt upgrade -y
 apt dist-upgrade -y
 apt autoremove -y
 curl -sL https://deb.nodesource.com/setup_10.x | sudo bash -
-apt install nodejs
+apt install -y nodejs
 ln -s /usr/bin/nodejs /usr/bin/node
 git clone https://github.com/roeniss/amazon-sqs-handson-for-non-cs
-mv sqs_test.service /lib/systemd/system/
-systemctl start sqs_test
-systemctl enable sqs_test
+cd amazon-sqs-handson-for-non-cs
+npm install
+mv sqs_node.service /lib/systemd/system/sqs_node.service
+systemctl start sqs_node
+systemctl enable sqs_node
 
